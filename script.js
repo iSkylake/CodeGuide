606718728f171c9bf2a66a6e4b5a561500fdcbb7
iSkylake <skylakebeta@gmail.com>
2018-02-23 17:09:57 -0800

    Add hover animation and update search function

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let container = document.getElementsByClassName("container");
 let searchLangInput = document.getElementById("search-language-input");
 
+// CREATE HTML ELEMENTS TO APPEND FUNCTION
+
 function displayAll(){
 	for(let i=0; i<languages.length; i++){
 		let langBox = document.createElement("div");
 		let langImg = document.createElement("img");
+		let langName = document.createElement("h1");
 
 		langBox.classList.add("lang-box");
 		langBox.classList.add(languages[i].color);
 
-		langBox.setAttribute("id", languages[i].name);
 		langImg.setAttribute("src", languages[i].url);
+		langName.appendChild(document.createTextNode(languages[i].name));
+		langBox.appendChild(langName);
 		langBox.appendChild(langImg);
 
 		container[0].appendChild(langBox);
@@ -19,10 +23,12 @@ function displayAll(){
 
 displayAll();
 
+// DYNAMIC SEARCH FUNCTION
+
 searchLangInput.addEventListener("keyup", function(){
 	let langElements = container[0].children;
 	for(let i=0; i<langElements.length; i++){
-		if(langElements[i].getAttribute("id").toLowerCase().indexOf(searchLangInput.value.toLowerCase()) > -1){
+		if(langElements[i].children[0].textContent.toLowerCase().indexOf(searchLangInput.value.toLowerCase()) > -1){
 			langElements[i].style.display = "";
 		} else {
 			langElements[i].style.display = "none";
