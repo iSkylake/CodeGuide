385e84ba3e62fa10829150ccb4c226096561a0d6
iSkylake <skylakebeta@gmail.com>
2018-03-22 13:31:42 -0700

    Add template function for JS page

--- js/jsscript.js
+++ js/jsscript.js
@@ -1,17 +1,44 @@
 let scriptContainer = document.getElementsByClassName("script-container");
+let copyIcon = document.getElementsByClassName("copy-icon");
+let scripts = document.getElementsByClassName("scripts");
 
-let scriptBlock = document.createElement("div");
-let scriptName = document.createElement("div");
-let scripts = document.createElement("code");
+// Display all JS scripts
+for(let i=0; i<javascript.length; i++){
+	let scriptBlock = document.createElement("div");
+	let scriptName = document.createElement("div");
+	let scripts = document.createElement("code");
 
-scriptBlock.className = "script-block";
-scriptName.className += "script-name script-name-js";
-scripts.className = 'scripts';
+	scriptBlock.className = "script-block";
+	scriptName.className += "script-name script-name-js";
+	scripts.className = 'scripts';
 
-scriptName.innerHTML = '<h3>ForEach</h3>'
-scripts.innerHTML = '<span class="js-reserved">let</span> arr <span class="js-symbols">=</span> [<span class="js-numbers">1</span>, <span class="js-numbers">2</span>, <span class="js-numbers">3</span>]; <br> <br> arr.<span class="js-reserved">forEach</span>(<span class="js-reserved">function</span>(<i><span class="js-params">num</span></i>){ <br> &nbsp&nbsp<span class="js-reserved">console</span>.<span class="js-reserved">log</span>(num<span class="js-symbols">+</span><span class="js-numbers">2</span>); <br> });';
+	scriptName.innerHTML = '<h3>' + javascript[i].name + '</h3>';
 
-scriptBlock.appendChild(scriptName);
-scriptBlock.appendChild(scripts);
+	for(let j=0; j<javascript[i].code.length; j++){
+		scripts.innerHTML += javascript[i].code[j];
+	}
+	scriptBlock.appendChild(scriptName);
+	scriptBlock.appendChild(scripts);
 
-scriptContainer[0].appendChild(scriptBlock);
+	// Create copy icon		
+	let newCopyIcon = document.createElement("i");
+	let tooltip = document.createElement("span");
+	tooltip.appendChild(document.createTextNode("Copy"));
+	newCopyIcon.className += "far fa-copy copy-icon";
+	tooltip.className = "tooltip";
+	newCopyIcon.appendChild(tooltip);
+	scriptBlock.appendChild(newCopyIcon);
+
+	scriptContainer[0].appendChild(scriptBlock);
+}
+
+// Copy functionality
+for(let i=0; i<copyIcon.length; i++){
+	copyIcon[i].addEventListener("click", function(){
+		var range = window.getSelection().getRangeAt(0);
+		range.selectNode(scripts[i]);
+		window.getSelection(range);
+		document.execCommand("copy");
+		// window.getSelection().removeAllRanges();
+	});
+}
