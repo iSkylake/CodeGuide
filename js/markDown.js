0895e6e13ece76f5f5f7031a26874029855a535e
iSkylake <skylakebeta@gmail.com>
2018-03-27 19:50:06 -0700

    Add Markdown page and view template

--- js/markDown.js
+++ js/markDown.js
@@ -3,19 +3,19 @@ let copyIcon = document.getElementsByClassName("copy-icon");
 let scripts = document.getElementsByClassName("scripts");
 
 // Display all JS scripts
-for(let i=0; i<css.length; i++){
+for(let i=0; i<markdown.length; i++){
 	let scriptBlock = document.createElement("div");
 	let scriptName = document.createElement("div");
 	let scripts = document.createElement("code");
 
 	scriptBlock.className = "script-block";
-	scriptName.className += "script-name script-name-css";
+	scriptName.className += "script-name script-name-markdown";
 	scripts.className = 'scripts';
 
-	scriptName.insertAdjacentHTML('beforeend', '<h3>' + css[i].name + '</h3>');
+	scriptName.insertAdjacentHTML('beforeend', '<h3>' + markdown[i].name + '</h3>');
 
-	for(let j=0; j<css[i].code.length; j++){
-		scripts.insertAdjacentHTML('beforeend', css[i].code[j]);
+	for(let j=0; j<markdown[i].code.length; j++){
+		scripts.insertAdjacentHTML('beforeend', markdown[i].code[j]);
 	}
 	scriptBlock.appendChild(scriptName);
 	scriptBlock.appendChild(scripts);
